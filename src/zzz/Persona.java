bf97c17ad886c224fd2bc9c1a971467765e6db7a
aborgeat01 <you@example.com>
2022-03-30 21:28:01 -0300

    devuelve apellido mayuscula

--- src/zzz/Persona.java
+++ src/zzz/Persona.java
@@ -4,4 +4,18 @@ public class Persona {
  
 	private String nombre;
 	private String apellido;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido.toUpperCase();
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
 }
